ec4198433fa5f709fed5e0962fe0868e6163df4c
oksei_admin <oksei_admin@gitflic.ru>
2023-04-07 22:49:03 +0500

    Добавьте файлы проекта.

---
+++ overOperators/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace overOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("pol");
+            Thread.Sleep(10000);
+            User user2 = new User("ron");
+
+            Console.WriteLine(user1 + "\n" + user2); 
+            Console.WriteLine(user1 < user2);
+        }
+    }
+}
---
+++ overOperators/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overOperators
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        public User(string n)
+        {
+            Name = n;
+            RegistrationDate = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return new string($"User: {Name} registered {RegistrationDate}");
+        }
+
+        public static bool operator > (User u1, User u2)
+        {
+            if (u1.RegistrationDate > u2.RegistrationDate) return true;
+            else return false;
+        }
+
+        public static bool operator < (User u1, User u2)
+        {
+            if (u1.RegistrationDate < u2.RegistrationDate) return true;
+            else return false;
+        }
+    }
+}
